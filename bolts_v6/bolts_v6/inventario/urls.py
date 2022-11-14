9104db15aef75caaabc5bd370e6331851743dfe1
Negidix <76134586+negidi@users.noreply.github.com>
2022-11-14 12:50:58 -0300

    Bolts V6
    Se agrega:
    Modulo consultas se encuentra OK (lista maqna/htas/clientes)
    Modulo Clientes --> Crea clientes
    Modulos maqnas/htas --> Crea/actualiza OK
    Modulo usuarios --> Genera error al agregar US
    Login --> se incorpora login (considerar que si se activa el login, no se pueden hacer transacciones en la BD)
    Se incorporan imagenes propuestas(faltan rutearlas en el carrusel)

---
+++ bolts_v6/bolts_v6/inventario/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from inventario import views
+from django.urls import path
+
+urlpatterns = [
+
+
+    path('', views.index),
+    path('crearHerramientas/', views.crearHerramientas),
+    path('registrarHerramienta/', views.registrarHerramienta),
+    path('crearMaquinaria/', views.crearMaquinaria),
+    path('registrarMaquinaria/', views.registrarMaquinaria),
+    path('actualizarmaq/', views.actualizarmaq),
+    path('editarmaq/', views.editarmaq),
+    path('editarmaq/editarmaq', views.editarmaq),
+    path('editarherr/', views.editarherr),
+    path('actualizarherr/', views.actualizarherr),
+    path('editarherr/editarherr', views.editarherr),
+    path('crearclientes/', views.crearclientes),
+    path('registrarCliente/', views.registrarCliente),
+    path('crearusuarios/', views.crearusuarios),
+    path('registrarUsuario/', views.registrarUsuario),
+    path('listarherramientas/', views.listarherramientas),
+    path('listarmaquinaria/', views.listarmaquinaria),
+    path('listarclientes/', views.listarclientes),
+    path ('home/', views.home),
+    path ('login/', views.login),
+    path ('login/homelogin', views.homelogin),
+
+
+]
